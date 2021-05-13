57078d54764bca950751ff778dac0f4fc70f57fb
accrazed <accrazed@gmail.com>
2021-05-13 00:25:30 -0400

    highlight support for settings button

--- src/SettingsButton.js
+++ src/SettingsButton.js
@@ -1,27 +1,27 @@
-import "react-native-gesture-handler";
-import React from "react";
-import { View, TouchableHighlight } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome";
-import { useNavigation } from "@react-navigation/native";
-import styles from "./Style";
+import 'react-native-gesture-handler';
+import React from 'react';
+import { View, TouchableHighlight } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { useNavigation } from '@react-navigation/native';
+import styles from './Style';
 
 const SettingsButton = () => {
-  const navigation = useNavigation();
-  return (
-    <TouchableHighlight
-      style={{ borderRadius: 30, overflow: "hidden", right: 10 }}
-      onPress={() => navigation.navigate("Settings")}
-    >
-      <View style={styles.SettingsButton}>
-        <Icon
-          style={(styles.IconCenter, styles.SettingsIcon)}
-          name="cog"
-          size={40}
-          color="white"
-        />
-      </View>
-    </TouchableHighlight>
-  );
+    const navigation = useNavigation();
+    return (
+        <TouchableHighlight
+            style={styles.SettingsButton}
+            onPress={() => navigation.navigate('Settings')}
+        >
+            <View style={styles.SettingsButton}>
+                <Icon
+                    style={(styles.IconCenter, styles.SettingsIcon)}
+                    name="cog"
+                    size={30}
+                    color="white"
+                />
+            </View>
+        </TouchableHighlight>
+    );
 };
 
 export default SettingsButton;
